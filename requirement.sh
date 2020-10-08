815c4c4183880b6284ec378e6c77cad6d37e1423
3l-tech <68188212+3l-tech@users.noreply.github.com>
2020-10-09 00:36:47 +0100

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,14 @@
+#
+#HACKER 3L-TECH
+#
+
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
